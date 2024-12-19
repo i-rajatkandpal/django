5b677ae75b6d9cb3f73fe2a499f6b33ee5c38df3
i-rajatkandpal <rajatkandpal178@gmail.com>
2024-12-20 01:43:34 +0530

    second commit

--- textutils/urls.py
+++ textutils/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('removepunc', views.removepunc, name='rempun'),
+    path('capfirst', views.capfirst, name='capfirst'),
+    path('newlineremove', views.newlineremove, name='newlineremove'),
+    path('spaceremove', views.spaceremove, name='spaceremove'),
+    path('charcount', views.charcount, name='charcount'),
+
 ]
+
+
+
+
+
--- textutils/views.py
+++ textutils/views.py
@@ -1,1 +1,29 @@
-#my files
+from django.http import HttpResponse
+
+def index(request):
+    html = """
+    <h1>Welcome to the Home Page</h1>
+    <ul>
+        <li><a href="removepunc">Remove Punctuation</a></li>
+        <li><a href="capfirst">Capitalize First</a></li>
+        <li><a href="newlineremove">Newline Remover</a></li>
+        <li><a href="spaceremove">Space Remover</a></li>
+        <li><a href="charcount">Character Count</a></li>
+    </ul>
+    """
+    return HttpResponse(html)
+
+def removepunc(request):
+    return HttpResponse('''Remove punctuation <br> <a href = "/"> back </a>''')
+
+def capfirst(request):
+    return HttpResponse('''Capitalize first <br> <a href = "/"> back </a>''')
+
+def newlineremove(request):
+    return HttpResponse('''Remove newlines <br> <a href = "/"> back </a>''')
+
+def spaceremove(request):
+    return HttpResponse('''Remove spaces <br> <a href = "/"> back </a>''')
+
+def charcount(request):
+    return HttpResponse('''Character count <br> <a href = "/"> back </a>''')
